1efd3a8abe9a980a284e3fa9118734db22cd94f8
Ravi <evenka@codeaurora.org>
2019-06-27 17:44:21 -0400

    msm: ais: Lock implementation for avoid race condition

--- drivers/media/platform/msm/ais/msm_vb2/msm_vb2.c
+++ drivers/media/platform/msm/ais/msm_vb2/msm_vb2.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -19,15 +19,19 @@ static int msm_vb2_queue_setup(struct vb2_queue *q,
 	unsigned int sizes[], void *alloc_ctxs[])
 {
 	int i;
-	struct msm_v4l2_format_data *data = q->drv_priv;
+	struct msm_v4l2_format_data *data = NULL;
+	int rc = -EINVAL;
+
+	mutex_lock(q->lock);
+	data = q->drv_priv;
 
 	if (!data) {
 		pr_err("%s: drv_priv NULL\n", __func__);
-		return -EINVAL;
+ 		goto done;
 	}
 	if (data->type == V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE) {
 		if (WARN_ON(data->num_planes > VIDEO_MAX_PLANES))
-			return -EINVAL;
+ 			goto done;
 
 		*num_planes = data->num_planes;
 
@@ -38,7 +42,11 @@ static int msm_vb2_queue_setup(struct vb2_queue *q,
 			   data->type);
 		return -EINVAL;
 	}
-	return 0;
+	rc = 0;
+
+done:
+	mutex_unlock(q->lock);
+	return rc;
 }
 
 static int msm_vb2_buf_init(struct vb2_buffer *vb)
