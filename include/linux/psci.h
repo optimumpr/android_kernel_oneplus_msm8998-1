3f3a8f96dfa393cb2c4a163abd8b1152d6059804
Marc Zyngier <marc.zyngier@arm.com>
2019-11-11 13:45:12 -0500

    firmware/psci: Expose SMCCC version through psci_ops

--- include/linux/psci.h
+++ include/linux/psci.h
@@ -33,6 +33,11 @@ enum psci_conduit {
 int psci_cpu_init_idle(unsigned int cpu);
 int psci_cpu_suspend_enter(unsigned long index);
 
+enum smccc_version {
+	SMCCC_VERSION_1_0,
+	SMCCC_VERSION_1_1,
+};
+
 struct psci_operations {
 	u32 (*get_version)(void);
 	int (*cpu_suspend)(u32 state, unsigned long entry_point);
@@ -43,6 +48,7 @@ struct psci_operations {
 			unsigned long lowest_affinity_level);
 	int (*migrate_info_type)(void);
 	enum psci_conduit conduit;
+	enum smccc_version smccc_version;
 };
 
 extern struct psci_operations psci_ops;
