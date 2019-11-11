03431ec7d2c07bc3875fff7f9d4956a1dea01388
Marc Zyngier <marc.zyngier@arm.com>
2019-11-11 13:35:14 -0500

    arm64: KVM: Report SMCCC_ARCH_WORKAROUND_1 BP hardening support

--- include/linux/arm-smccc.h
+++ include/linux/arm-smccc.h
@@ -69,6 +69,10 @@
 	ARM_SMCCC_CALL_VAL(ARM_SMCCC_FAST_CALL,				\
 			   ARM_SMCCC_SMC_32,				\
 			   0, 1)
+#define ARM_SMCCC_ARCH_WORKAROUND_1					\
+	ARM_SMCCC_CALL_VAL(ARM_SMCCC_FAST_CALL,				\
+			   ARM_SMCCC_SMC_32,				\
+			   0, 0x8000)
 
 #ifndef __ASSEMBLY__
 
