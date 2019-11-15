bfa07b81b3fbd7953ef877c8a41fc85a50587056
Albere <1OptimizerPro@gmail.com>
2019-11-15 09:53:57 -0500

    Revert "fix crypto gf128mul"
    This reverts commit 9092279ea503e8d1dde7b46bcff6571439774dc2.

--- include/crypto/gf128mul.h
+++ include/crypto/gf128mul.h
@@ -160,8 +160,8 @@
  *  It multiplies a and b and puts the result in a
  */
 void gf128mul_lle(be128 *a, const be128 *b);
-
 void gf128mul_bbe(be128 *a, const be128 *b);
+void gf128mul_ble(be128 *a, const be128 *b);
 
 /*
  * The following functions multiply a field element by x in
@@ -233,7 +233,8 @@ static inline void gf128mul_free_4k(struct gf128mul_4k *t)
 	kzfree(t);
 }
 
-/* 64k table optimization, implemented for bbe */
+
+/* 64k table optimization, implemented for lle, ble, and bbe */
 
 struct gf128mul_64k {
 	struct gf128mul_4k *t[16];
@@ -247,6 +248,7 @@ struct gf128mul_64k {
 struct gf128mul_64k *gf128mul_init_64k_lle(const be128 *g);
 struct gf128mul_64k *gf128mul_init_64k_bbe(const be128 *g);
 void gf128mul_free_64k(struct gf128mul_64k *t);
+void gf128mul_64k_lle(be128 *a, struct gf128mul_64k *t);
 void gf128mul_64k_bbe(be128 *a, struct gf128mul_64k *t);
 
 #endif /* _CRYPTO_GF128MUL_H */
