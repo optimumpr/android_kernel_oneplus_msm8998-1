66add54d01ff63f7990779ce4566c3da4d1544e2
Oliver Neukum <oneukum@suse.com>
2019-11-25 20:27:19 -0500

    ax88172a: fix information leak on short answers

--- drivers/net/usb/ax88172a.c
+++ drivers/net/usb/ax88172a.c
@@ -226,7 +226,6 @@ err:
 	return ret;
 }
 
-
 static int ax88172a_bind(struct usbnet *dev, struct usb_interface *intf)
 {
 	int ret;
@@ -243,7 +242,7 @@ static int ax88172a_bind(struct usbnet *dev, struct usb_interface *intf)
 
 	/* Get the MAC address */
 	ret = asix_read_cmd(dev, AX_CMD_READ_NODE_ID, 0, 0, ETH_ALEN, buf);
-	if (ret < 0) {
+	if (ret < ETH_ALEN) {
 		netdev_err(dev->net, "Failed to read MAC address: %d\n", ret);
 		goto free;
 	}
